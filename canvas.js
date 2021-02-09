98f6e277bdb87de420f96020e0b85808f4e7b4fc
Pedro <pfernandez@payall.com.ve>
2021-02-08 22:51:48 -0400

    Corregir error del canvas del lado rojo
    Se corrigio el error del canvas en el segundo ciclo, del lado rojo

--- canvas.js
+++ canvas.js
@@ -16,25 +16,28 @@ function paintinglines(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.closePath();
 }
 
-function paintforclick()
-{
-{
+function paintforclick(){
+
     var lines = parseInt (texto.value);
     var l = 0;
     var yi, xf, xi, yf;
-}
-        while(l < lines)
-{
-    yi = 10 * (l + 1);
-    xf = 10 * (l + 1);
-    paintinglines ("#AAF", 0, yi, xf, 300);
-    console.log ("linea" + l);
-    l++
-}
-    for(l=0; l < lines; l++)
-{
-    yi = 10 * (l + 1);
-    yf = 10 * (l + 1);
-    paintinglines ("red", 0, yi, 300, yf);
-}
+
+    while(l < lines){
+
+        yi = 10 * (l + 1);
+        xf = 10 * (l + 1);
+        paintinglines ("#AAF", 0, yi, xf, 300);
+        console.log ("linea" + l);
+        l++
+        
+    }
+
+    for(l=0; l < lines; l++){
+
+        xi = 10 * (l + 1);
+        yf = 10 * (l + 1);
+        paintinglines ("red", xi, 0, 300, yf);
+
+    }
+
 }
